fa6952b3f8a31d95e864d8020951e38f768dc89d
LaXnZ <sumudithalanz@gmail.com>
2024-01-13 00:26:14 +0530

    toolbar with logout functionality

--- app/src/main/java/com/example/note_taking_app/notesactivity.java
+++ app/src/main/java/com/example/note_taking_app/notesactivity.java
@@ -1,14 +1,71 @@
 package com.example.note_taking_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+//import android.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class notesactivity extends AppCompatActivity {
+    FloatingActionButton mcreatenotesfab;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notesactivity);
+
+        mcreatenotesfab = findViewById(R.id.createnotefab);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("All Notes");
+        }
+
+
+
+        mcreatenotesfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(notesactivity.this, createnote.class));
+            }
+        });
     }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(notesactivity.this, MainActivity.class));
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
